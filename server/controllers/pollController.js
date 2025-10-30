f29c998675b3feac7d35a381472a9f68697c808a
Aayan Lakhani <alakhani2@student.ohlone.edu>
2025-10-29 18:11:59 -0700

    Workshop 2 Completed

--- server/controllers/pollController.js
+++ server/controllers/pollController.js
@@ -3,12 +3,43 @@ const Poll = require("../models/Poll");
 // TODO
 // ACTIVITY 2a - Implement getPolls function (return all polls)
 
+const getPolls = async ( ) => {
+    const poll = await Poll.find();
+    console.log("Returning polls list");
+    return poll;
+}
+
+module.exports = {getPolls}
+
 // ACTIVITY 2b - Implement getPoll function (get one poll by id)
+
+const getPoll = async (param) => {
+    const { id } = param;
+    const poll = await Poll.find({id : id});
+    console.log('Fetching poll', param);
+    return poll;
+}
+
+module.exports = {getPoll}
 
 // END ACTIVITY 2
 
 // TODO
 // ACTIVITY 3a - Implement postPoll function to create a new poll
+
+const postPoll = async({ ownerId, title, description, options }) => {
+    if(!ownerId || !title || !options) return;
+
+    const poll = new Poll({
+        ownerId : ownerId,
+        title: title,
+        description: description,
+        options: options,
+    })
+
+    await poll.save();
+    return poll;
+}
 
 // ACTIVITY 3b - Implement postVote function to cast a vote
 
